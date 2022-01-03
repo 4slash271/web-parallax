c3a05aab665007114042953a676d889ee2a6db7f
“4slash271” <4slash271@gmail.com>
2022-01-04 02:04:01 +0900

    pf-wrapper finished

--- js/animation.js
+++ js/animation.js
@@ -2,8 +2,8 @@
 	$(this).toggleClass('active')
 }); */
 
-var count = 0;
-var seconds = 0;
+var count = 0;//박스를 클릭한 횟수를 셈
+var seconds = 0;//흐른 시간(초)
 
 $('.box1').click(function(){
 	count = count + 1;
@@ -18,7 +18,7 @@ $('.box1').click(function(){
 $('#second').change(function(){
 	$('.box1').css('animation-duration', $(this).val())
 	$('.box1').css('animation-play-state', 'running')
-	setInterval(function() {
+	setInterval(function() {//정해진 시간마다 함수 실행(1초마다 카운트)
 		seconds = seconds + 1;
 	}, 1)
 })
