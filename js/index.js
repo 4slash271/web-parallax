309c51cddac3e6fb6ec4511a6160ee2d5a9a75af
“4slash271” <4slash271@gmail.com>
2022-01-05 03:26:33 +0900

    pf-wrapper and modal-wrapper finished

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 
-
+    let modal = document.querySelector(".modal-wrapper");
+    let pfPics = document.querySelectorAll(".pf-wrap > .pf > img");
 function moveHeader(){
    let scrollTop =document.documentElement.scrollTop;
  
@@ -17,8 +18,40 @@ else {
 function toggleNavi(){
     document.querySelector(".mb-navi-wrapper").classList.toggle('active');
 }
+function openModal(){
+    let src = this.getAttribute("src");
+    let modalImg = document.querySelector(".modal-wrap .modal-img");
+    let modalWrap = document.querySelector(".modal-wrap");
+    modalImg.setAttribute("src",src);
+    modal.style.display = "block";
+    modalWrap.style.transform = "scale(0)";
+    modalWrap.style.transform = "scale(1)";
+    console.log(this);
+ 
+}
+
+
+pfPics.forEach((pfPics) => {
+    pfPics.addEventListener('click',openModal);
+  });
+  
+function closeModal(){
+    modal.style.display = "none";
+}
+
+var cols = document.querySelectorAll('#cols .btn');  
+		
+		[].forEach.call(cols,function(col){
+			col.addEventListener("click",click,false); 
+		});
+
+		function click(e){
+			window.alert(this.innerHTML);
+		}
 
 
 
 window.addEventListener('scroll', moveHeader);
 document.querySelector(".mb-bars").addEventListener('click',toggleNavi);
+modal.addEventListener('click',closeModal);
+
